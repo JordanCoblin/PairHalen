57b145b481082e1ed975ea6badb8be5cbf3c0b58
Jordan Coblin <jcoblin@jcoblin-mac.tsi.lan>
2017-11-10 15:43:56 -0800

    Clicking to pair/unpair working, UI updates accordingly

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, Button, TextInput, View, Image } from 'react-native';
 
 class TeamMember extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPairing: false
+    };
+  }
+
   render() {
     return (
       <View style={styles.teamMember} >
-        <Text style={styles.teamMemberFont}> {this.props.name} </Text>
+        <Button 
+        onPress={() => { this.props.handler(this.props.name) }}
+        style={styles.teamMemberFont} 
+        title = {this.props.name} />
       </View>
     );
   }
@@ -13,8 +24,56 @@ class TeamMember extends Component {
 
 
 export default class ProjectDisco extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nonPairingMembers : ["An", "Francisco", "Kayle"],
+      pairingMembers : []
+    };
+  }
+
+  onPressHandler = (txt) => {
+    let nonPairingMembers = this.state.nonPairingMembers;
+    let pairingMembers = this.state.pairingMembers;
+    let index = nonPairingMembers.indexOf(txt);
+
+    // member was no previously not pairing, move to pairing
+    if (index >= 0) {
+      nonPairingMembers.splice(index, 1);
+      pairingMembers.push(txt);
+    } 
+
+    // member was previously pairing, move to non-pairing
+    else {
+      index = pairingMembers.indexOf(txt);
+      pairingMembers.splice(index, 1);
+      nonPairingMembers.push(txt);
+    }
+
+    let newState = {}
+    newState["nonPairingMembers"] = nonPairingMembers;
+    newState["pairingMembers"] = pairingMembers;
+
+    this.setState(newState)
+  }
 
   render() {
+
+    let nonPairingMembers = this.state.nonPairingMembers;
+    var nonPairingMembersViews = [];
+
+    for (var i = 0; i < nonPairingMembers.length; i++) {
+        nonPairingMembersViews.push(<TeamMember name={nonPairingMembers[i]} handler={this.onPressHandler} key={i} />);
+    }
+
+    let pairingMembers = this.state.pairingMembers;
+    var pairingMembersViews = [];
+
+    for (var i = 0; i < pairingMembers.length; i++) {
+        pairingMembersViews.push(<TeamMember name={pairingMembers[i]} handler={this.onPressHandler} key={i} />);
+    }
+
     return (
       <View style={{flex: 1}}>
 
@@ -25,14 +84,10 @@ export default class ProjectDisco extends Component {
 
         <View style={{flex: 2, flexDirection: 'row'}} >
           <View style={{flex: 1, backgroundColor: 'steelblue', justifyContent: 'space-around'}} >
-            <TeamMember name="An" />
-            <TeamMember name="Francisco" />
-            <TeamMember name="Kayle" />
+            {nonPairingMembersViews}
           </View>
-
-          <View 
-          style={{flex: 1, backgroundColor: 'skyblue', alignItems:'center', justifyContent:'center'}} >
-            <Text style = {styles.teamMemberFont}> Drag over here </Text>
+          <View style={{flex: 1, backgroundColor: 'skyblue', alignItems:'center', justifyContent:'center'}} >
+            {pairingMembersViews}
           </View>
 
         </View>
